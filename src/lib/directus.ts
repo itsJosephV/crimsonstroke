156dbf56cc308c95e4651cb71ef624c140a89835
Joseph <jvp.2703@gmail.com>
2024-12-05 18:47:44 +0000

    check directus url

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,3 +1,7 @@
-import { createDirectus, rest } from "@directus/sdk"
+if (!process.env.DIRECTUS_API_ENDPOINT) {
+  throw new Error("DIRECTUS_API_ENDPOINT is not defined in the environment variables.");
+}
 
-export const directus = createDirectus(process.env.DIRECTUS_API_ENDPOINT as string).with(rest())
+import { createDirectus, rest } from "@directus/sdk";
+
+export const directus = createDirectus(process.env.DIRECTUS_API_ENDPOINT as string).with(rest());
