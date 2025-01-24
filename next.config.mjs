f52fdc2882934ad1757f43f043f0320bce302512
Joseph <jvp.2703@gmail.com>
2025-01-24 00:52:41 +0000

    refactor: clean up codebase by removing unused files and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
-  logging: {
-    fetches: {
-      fullUrl: true,
-    },
-  },
-  async headers() {
-    return process.env.NODE_ENV === "development"
-      ? []
-      : [
-          {
-            source: "/(.*)",
-            headers: [
-              {
-                key: "Content-Security-Policy",
-                value: `frame-src 'self' ${process.env.CONTENT_SECURITY_POLICY_DIRECTIVES__FRAME_SRC};`,
-              },
-            ],
-          },
-        ];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.DIRECTUS_API_ENDPOINT
-          ? process.env.DIRECTUS_API_ENDPOINT.replace("https://", "")
-          : "",
-      },
-    ],
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,7 @@
-import Video from "@/components/video";
-import { getHome } from "@/lib/home";
-import { getNoticias } from "@/lib/noticias";
-import { unstable_cache } from "next/cache";
-import Image from "next/image";
-import Link from "next/link";
-
-export default async function Home() {
-  const data = await getHome();
-
-  const getCatchedNews = unstable_cache(getNoticias, ["noticias"], {
-    tags: ["noticias"],
-    // revalidate: 60,
-  });
-  const noticias = await getCatchedNews();
-
-  // console.log(noticias);
-
-  // console.log(noticias, "noticiassss");
-
+export default function Home() {
   return (
-    <div className="p-10">
-      <div className="mb-5">
-        <h1 className="text-3xl">{data.hero_title}</h1>
-        <p className="text-slate-400">{data.hero_subtitle}</p>
-      </div>
-      <div>
-        <Video />
-      </div>
-
-      {data.hero_cover && (
-        <Image
-          width={200}
-          height={200}
-          src={`${process.env.DIRECTUS_API_ENDPOINT}/assets/${data.hero_cover}`}
-          alt=""
-        />
-      )}
-
-      {Array.isArray(data.hero_buttons) && (
-        <ul>
-          {data.hero_buttons.map((b) => (
-            <li key={b.label}>
-              <Link
-                className="bg-violet-600 inline-block py-2 px-4 rounded-md font-medium hover:bg-green-700 transition-colors duration-300"
-                href={b.link}
-              >
-                {b.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-      <div className="bg-slate-900 p-5 rounded-md mt-10">
-        <p>Noticias</p>
-        {Array.isArray(noticias) &&
-          noticias.map((n) => {
-            return (
-              <Link href={`noticias/${n.id}`} key={n.id}>
-                <div className="bg-violet-950 w-full p-3 mt-3">
-                  <h2>{n.titulo}</h2>
-                  <p className="mt-3">{n.descripcion}</p>
-                </div>
-                <Image
-                  width={300}
-                  height={300}
-                  src={`${process.env.DIRECTUS_API_ENDPOINT}/assets/${n.imagen}`}
-                  alt="test-image"
-                />
-              </Link>
-            );
-          })}
-      </div>
+    <div>
+      <h1>Illustrations Portfolio Project</h1>
     </div>
   );
 }
