9d95f85fe7ae3acc38af748ee007dff979674445
Joseph <jvp.2703@gmail.com>
2025-01-24 09:07:48 +0000

    feat: enhance metadata generation for news page with dynamic image URL

--- src/app/noticias/[slug]/page.tsx
+++ src/app/noticias/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import ShareButton from "@/components/ShareButton";
+import type { Metadata } from "next";
 
 // Función para obtener datos del post (simulación)
 async function getPost(slug: string) {
@@ -15,29 +16,39 @@ export async function generateMetadata({
   params,
 }: {
   params: { slug: string };
-}) {
+}): Promise<Metadata> {
   const post = await getPost(params.slug);
 
   return {
     title: post.title,
     description: post.description,
     openGraph: {
-      title: post.title,
-      description: post.description,
-      url: `https://crimsonstroke.xyz/noticias/${params.slug}`, // URL absoluta
-      type: "article",
       images: [
         {
-          url: "https://crimsonstroke.xyz/og-image.jpg", // Reemplaza con tu imagen
+          url: "https://images.unsplash.com/photo-1737405555489-78b3755eaa81?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           width: 1200,
           height: 630,
         },
       ],
     },
+    // openGraph: {
+    //   title: post.title,
+    //   description: post.description,
+    //   url: `https://crimsonstroke.xyz/noticias/${params.slug}`, // URL absoluta
+    //   type: "article",
+    //   images: [
+    //     {
+    //       url: "https://crimsonstroke.xyz/og-image.jpg", // Reemplaza con tu imagen
+    //       width: 1200,
+    //       height: 630,
+    //     },
+    //   ],
+    // },
   };
 }
 
 const Page = async ({ params }: { params: { slug: string } }) => {
+  console.log(params.slug);
   const post = await getPost(params.slug);
   const absoluteUrl = `https://crimsonstroke.xyz/noticias/${params.slug}`;
 
